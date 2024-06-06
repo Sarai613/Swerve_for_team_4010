bac381b57e78339c1ebb92a8d58c1e9e2926b95a
DARR-1 <a01665821@tec.mx>
2024-06-06 13:32:18 -0600

    adjust constants

--- Swerve_teleop/src/main/java/frc/robot/utilidades/Constants.java
+++ Swerve_teleop/src/main/java/frc/robot/utilidades/Constants.java
@@ -3,14 +3,14 @@ package frc.robot.utilidades;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class Constants {
-    public static final double MAX_SPEED = 20; // in meters per second
-    public static final double DISTANCE_BETWEEN_WHEELS_AND_THE_CENTER = 0.381;
+    public static final double MAX_SPEED = 3.657599994440448; // in meters per second
+    public static final double DISTANCE_BETWEEN_WHEELS_AND_THE_CENTER = 28;
     public static final double WHEEL_DIAMETER = 20; //in mm
-    public static final double DISTANCE_PER_ROTATION = Math.PI * WHEEL_DIAMETER * 1000; //in meters
-    public static final double TURNING_ENCODER_ROT_2_RAD = 2 * Math.PI;
-    public static final double TURNING_ENCODER_RPM_2_RAD_PER_SECOND = 1 * Math.PI;
+    public static final double DISTANCE_PER_ROTATION = Math.PI * WHEEL_DIAMETER * 1000 / 8.14; //in meters
+    public static final double TURNING_ENCODER_ROT_2_RAD = 1/12.8 * 2 * Math.PI;
+    public static final double TURNING_ENCODER_RPM_2_RAD_PER_SECOND = 8.6 * TURNING_ENCODER_ROT_2_RAD;
     public static final double P = 0.5; //PID P value
-    public static final double JOYSTICK_DEADZONE = 0.05;//this value most be beetwen 0 and 1
+    public static final double JOYSTICK_DEADZONE = 0.1;//this value most be beetwen 0 and 1
 
 
     // Locations for the swerve drive modules relative to the robot center.
