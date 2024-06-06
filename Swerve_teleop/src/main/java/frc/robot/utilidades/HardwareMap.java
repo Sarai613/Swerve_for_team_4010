8585ddcac995232f92ef36ff87d253edf9d8e346
DARR-1 <a01665821@tec.mx>
2024-06-05 19:34:14 -0600

    Put the correct pins in HardwareMap class

--- Swerve_teleop/src/main/java/frc/robot/utilidades/HardwareMap.java
+++ Swerve_teleop/src/main/java/frc/robot/utilidades/HardwareMap.java
@@ -3,7 +3,7 @@ package frc.robot.utilidades;
 import frc.robot.swerve.SwerveModule;
 public class HardwareMap {
     public static final SwerveModule frontLeft = new SwerveModule(23, 32);
-    public static final SwerveModule frontRight = new SwerveModule(24, 22);
-    public static final SwerveModule backLeft = new SwerveModule(31, 21);
-    public static final SwerveModule backRight = new SwerveModule(14, 33);
+    public static final SwerveModule frontRight = new SwerveModule(22, 24);
+    public static final SwerveModule backLeft = new SwerveModule(21, 31);
+    public static final SwerveModule backRight = new SwerveModule(33, 34);
 }
