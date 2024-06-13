fe2158a2a8db755d7004edffa62191232fb2135b
DARR-1 <151085951+DARR-1@users.noreply.github.com>
2024-06-13 13:23:45 -0600

    adjust constants(again)

--- Swerve_teleop/src/main/java/frc/robot/utilidades/HardwareMap.java
+++ Swerve_teleop/src/main/java/frc/robot/utilidades/HardwareMap.java
@@ -4,8 +4,8 @@ import frc.robot.swerve.SwerveModule;
 public class HardwareMap{
 
     //Defines every single module by giving the drive spark id, the turning spark id, the absolute encoder id, absolute encoder offset
-    public static final SwerveModule frontLeft = new SwerveModule(23, 32, 20, 0, true);
-    public static final SwerveModule frontRight = new SwerveModule(22, 24, 21, 0, false);
-    public static final SwerveModule backLeft = new SwerveModule(21, 31, 22, 0, false );
-    public static final SwerveModule backRight = new SwerveModule(33, 34, 23, 0, true);
+    public static final SwerveModule frontLeft = new SwerveModule(23, 32, 13, 0, true);
+    public static final SwerveModule frontRight = new SwerveModule(22, 24, 12, 0, false);
+    public static final SwerveModule backLeft = new SwerveModule(21, 31, 10, 0, false );
+    public static final SwerveModule backRight = new SwerveModule(33, 34, 11, 0, true);
 }
